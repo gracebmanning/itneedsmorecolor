ca59962de6360792349b74da60e2e125c7580264
Grace Manning <gracebmanning@gmail.com>
2024-01-24 00:13:59 -0800

    start adding about page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,18 +4,30 @@ import folder from '../assets/icons/directory_favorites-2.png';
 import search from '../assets/icons/search_directory-0.png';
 
 function Main() {
+  function toggleAbout(){
+    let elem = document.getElementById("about-circle");
+    if(elem.style.display === "flex"){
+      elem.style.display = "none"
+    }
+    else{
+      elem.style.display = "flex";
+    }
+  }
   return (
     <div className="main">
       <div className="content">
-        <div className="circle">
+        <div id="home-circle">
             <h1 className="title">itneedsmorecolor.com</h1>
+        </div>
+        <div id="about-circle">
+          <p>About. <a href="google.com">Link doesn't work :(</a></p>
         </div>
         <div className="nav-buttons">
             <div className="nav-button" id="proj-icon">
               <img className="nav-icon" src={folder} alt="Windows 98 folder icon with blue asterisk on it." />
               <p className="nav-text">projects</p>
             </div>
-            <div className="nav-button" id="about-icon">
+            <div className="nav-button" id="about-icon" onClick={toggleAbout}>
               <img className="nav-icon" src={search} alt="Windows 98 folder icon with magnifying glass over it." />
               <p className="nav-text">about</p>
             </div>
