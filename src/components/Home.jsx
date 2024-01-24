646ae33c378d2bbbc41e65975970f4c186cada5a
Grace Manning <gracebmanning@gmail.com>
2024-01-23 17:54:06 -0800

    initial commit, cirle image & text

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="circle">
+            
+            <p className="title">itneedsmorecolor.com</p>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
