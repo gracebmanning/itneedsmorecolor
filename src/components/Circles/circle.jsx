bdc9870734df799415b8056bc13d3c94f02cc810
Grace Manning <gracebmanning@gmail.com>
2025-01-12 19:33:40 -0800

    remove details from site

--- src/components/Circles/circle.jsx
+++ src/components/Circles/circle.jsx
@@ -18,7 +18,7 @@ export function ProjectCircle(){
       <h1>projects</h1>
       <ul id="projects-list">
         
-        <li><a className="proj-list-link" href="/projects-gallery">experimenting / other projects</a></li>
+        <li><a className="proj-list-link" href="/projects-gallery">sample project page</a></li>
       </ul>
     </div>
   )
@@ -28,7 +28,7 @@ export function AboutCircle(){
   return(
     <div className="circle" id="about-circle">
       <h1>about me</h1>
-      <p id="about-text">this website is unfinished!!</p>
+      <p id="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales rutrum dignissim. Etiam enim velit, dapibus non tortor vel, eleifend sollicitudin nibh. Nunc fringilla imperdiet erat nec finibus. Maecenas mattis commodo arcu, quis ultricies magna dapibus eget. Vestibulum euismod lobortis vulputate.<br/><br/>Mauris commodo leo in ante laoreet tincidunt. Morbi sed lobortis nulla, luctus posuere nulla. Cras a est aliquet leo finibus pretium. Maecenas posuere mi ac velit pellentesque molestie. Maecenas eget dapibus erat.</p>
     </div>
   );
 }
@@ -37,7 +37,7 @@ export function ContactCircle(){
   return(
     <div className="circle" id="contact-circle">
       <h1>contact</h1>
-      <p id="contact-text">want to work together or commission work from me? contact me via email at grace@itneedsmorecolor.com</p>
+      <p id="contact-text">contact me at [email address] or find me on social media at [username]!</p>
     </div>
   );
 }
